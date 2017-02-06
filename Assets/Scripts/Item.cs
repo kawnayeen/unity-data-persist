d0823d70d84d993abd98dbf80b676701793b48d5
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-02-06 21:15:10 +0600

    initial commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿[System.Serializable]
+public class Item{
+	public string name;
+	public int price;
+	public int amount;
+
+	public Item(string name, int price){
+		this.name = name;
+		this.price = price;
+	}
+
+	public void bought(){
+		this.amount++;
+	}
+
+	public void used(){
+		if (amount > 0)
+			amount--;
+	}
+}
---
+++ Assets/Scripts/ItemInventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Items", menuName = "Data/Item", order = 1)]
+public class ItemInventory : ScriptableObject {
+	public List<Item> itemList;
+}
